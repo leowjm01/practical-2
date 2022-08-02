5dff3dfd4ab8641d4073d42fe1a460bd7f527d83
anreh <anurehka@gmail.com>
2022-08-02 10:50:08 +0800

    week 5 - Practical 2

--- app/src/main/java/com/example/prac4/MainActivity.kt
+++ app/src/main/java/com/example/prac4/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +20,13 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.boxThree)
         val boxFourText = findViewById<TextView>(R.id.boxFour)
         val boxFiveText = findViewById<TextView>(R.id.boxFive)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
 
         val clickableView: List<View> =
             listOf(
-                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText
+                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, redButton, yellowButton, greenButton
             )
 
         for (item in clickableView) {
@@ -38,6 +42,9 @@ class MainActivity : AppCompatActivity() {
             R.id.boxThree -> view.setBackgroundColor(Color.CYAN)
             R.id.boxFour -> view.setBackgroundColor(Color.MAGENTA)
             R.id.boxFive -> view.setBackgroundColor(Color.YELLOW)
+            R.id.red_button-> view.setBackgroundColor(Color.RED)
+            R.id.yellow_button-> view.setBackgroundColor(Color.YELLOW)
+            R.id.green_button-> view.setBackgroundColor(Color.GREEN)
             else -> view.setBackgroundColor(Color.GRAY)
         }
     }
